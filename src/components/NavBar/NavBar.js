a9630e5294d360ffeebdc12f86a3342814aaba18
fahimahmedkhan <rolx9999@gmail.com>
2022-10-25 03:48:16 +0600

    Active Route(NavLink)

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import logo from '../../utilities/images/logo.png';
 
+
 const NavBar = () => {
+    const navLinkStyles = ({ isActive }) => {
+        return {
+            color: isActive ? 'orange' : 'black',
+        }
+    }
     return (
         <nav className='px-20 py-2 text-xl font-bold border-b-4 border-zinc-300 flex items-center justify-between'>
             <div className='flex items-center justify-between space-x-32'>
                 <div className='flex items-center justify-betweens space-x-4'>
                     <img src={logo} alt="" className='h-16 w-16 rounded-full' />
-                    <Link to='/' className='hover:text-orange-300'><h1 className='text-2xl'>Tutorial Academy</h1></Link>
+                    <NavLink to='/' className='hover:text-orange-300'><h1 className='text-2xl'>Tutorial Academy</h1></NavLink>
                 </div>
                 <div className='flex items-center justify-betweens space-x-20'>
-                    <Link to='/' className='hover:text-orange-300'>Home</Link>
-                    <Link to='/courses' className='hover:text-orange-300'>Courses</Link>
-                    <Link to='/faq' className='hover:text-orange-300'>FAQ</Link>
-                    <Link to='/blog' className='hover:text-orange-300'>Blog</Link>
+                    <NavLink style={navLinkStyles} to='/' className='hover:text-orange-300'>Home</NavLink>
+                    <NavLink style={navLinkStyles} to='/courses' className='hover:text-orange-300'>Courses</NavLink>
+                    <NavLink style={navLinkStyles} to='/faq' className='hover:text-orange-300'>FAQ</NavLink>
+                    <NavLink style={navLinkStyles} to='/blog' className='hover:text-orange-300'>Blog</NavLink>
                 </div>
             </div>
             <div className='flex items-center justify-betweens space-x-5'>
                 <div>Dark/Light</div>
                 <div>
-                    <Link to='/login' className='hover:text-orange-300'>Login</Link>
+                    <NavLink style={navLinkStyles} to='/login' className='hover:text-orange-300'>Login</NavLink>
                 </div>
             </div>
         </nav>
